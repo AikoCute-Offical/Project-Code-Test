40f8a93c9ae65f5aa91af4e32b7b548d87542e67
AikoCute <aikocute@icloud.com>
2022-10-01 23:39:50 +0700

    update

---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func dowfile() {
+	// tạo file mới
+	file, err := os.Create("Win 10 LTSC by Aiko.iso")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer file.Close()
+
+	// tải file
+	resp, err := http.Get("https://go.microsoft.com/fwlink/p/?LinkID=2195404&clcid=0x409&culture=en-us&country=US")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer resp.Body.Close()
+
+	// đọc file
+	_, err = io.Copy(file, resp.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+// hiển thị thưc thời gian tải file
+func progress() {
+	for {
+		fmt.Print(".")
+	}
+}
+
+func main() {
+	go dowfile()
+	go progress()
+	var input string
+	fmt.Scanln(&input)
+}
